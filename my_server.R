fc48fad3c5e410c96071a7058824fef334d6eb8e
a-hsiao <ahsiao2@uw.edu>
2022-03-10 02:03:55 -0800

    Have intro, conclusion and kind of plot1

--- my_server.R
+++ my_server.R
@@ -1,8 +1,28 @@
-# library(ggplot2)
-# library(plotly)
-# library(shiny)
-# library(bslib)
-# library(dplyr)
+library(ggplot2)
+library(plotly)
+library(shiny)
+library(bslib)
+library(dplyr)
+
+all_deaths <- read.csv('all_deaths.csv')
+
+all_deaths <- all_deaths %>%
+  mutate(race_full = case_when(race == "B" ~ 'Black',
+                               race == "W" ~ 'white',
+                               race == "H" ~ 'Hispanic',
+                               race == "A" ~ 'Asian',
+                               race == "AI" ~ 'Native American/Indigenous',
+                               race == "O" ~ 'Other',
+                               race == "NA" ~ 'Not Stated'))
+
+all_deaths <- all_deaths %>%
+  mutate(cause_death_full = case_when(cause_short == "M" | cause_short == "m" ~ 'Medical',
+                                      cause_short == "S" ~ 'Suicide',
+                                      cause_short == "DA" ~ 'Drug Abuse',
+                                      cause_short == "H" ~ 'Homicide',
+                                      cause_short == "O" ~ 'Other',
+                                      cause_short == "A" | cause_short == "AC" ~ 'Accident',
+                                      cause_short == "U" ~ 'Undetermined'))
 
 
 my_server <- function(input, output) {
@@ -18,6 +38,31 @@ my_server <- function(input, output) {
     # Return the value to be rendered by the UI
     message_str
   })
+  # output$hotdogPlot <- renderPlotly({
+  #   # Make a scatter plot of the number of hot dogs eaten over time
+  #   # Allow the user to select the color category
+  #   my_plot <- ggplot(data = hotdog_df) +
+  #     geom_point(mapping = aes(x = year, y = num_eaten, color= get(input$user_category))) +
+  #     
+  #     labs(title = "Title", color = custom_legend_titles[[input$user_category]])
+  #   
+  #   # Make interactive plot
+  #   # Remove mode bar
+  #   my_plotly_plot <- ggplotly(my_plot) %>%
+  #     config(displayModeBar = FALSE)
+  #   
+  #   return(my_plotly_plot)
+  # })
+  output$race_death <- renderPlotly ({
+    
+    race_bar_chart <- all_deaths %>%
+    ggplot(mapping = aes(x = race, fill = race)) +
+    labs(title = "Breakdown of Incarcerated Races", subtitle = "Who Have Died in Prison") +
+    geom_bar()
+  Plot <- ggplotly(race_bar_chart) %>%
+    config(displayModeBar = FALSE)
+  return(Plot)
+  })
 }
 
 
@@ -32,23 +77,23 @@ my_server <- function(input, output) {
 # 
 # # custom_titles <- create custom titles
 # 
-# # server <- function(input, output) {
-# # 
-# #   output$hotdogPlot <- renderPlotly({
-# #     # Make a scatter plot of the number of hot dogs eaten over time
-# #     # Allow the user to select the color category
-# #     my_plot <- ggplot(data = hotdog_df) +
-# #       geom_point(mapping = aes(x = year, y = num_eaten, color= get(input$user_category))) +
-# # 
-# #       labs(title = "Title", color = custom_legend_titles[[input$user_category]])
-# # 
-# #     # Make interactive plot
-# #     # Remove mode bar
-# #     my_plotly_plot <- ggplotly(my_plot) %>%
-# #       config(displayModeBar = FALSE)
-# # 
-# #     return(my_plotly_plot)
-# #   })
+# server <- function(input, output) {
+
+  # output$hotdogPlot <- renderPlotly({
+  #   # Make a scatter plot of the number of hot dogs eaten over time
+  #   # Allow the user to select the color category
+  #   my_plot <- ggplot(data = hotdog_df) +
+  #     geom_point(mapping = aes(x = year, y = num_eaten, color= get(input$user_category))) +
+  # 
+  #     labs(title = "Title", color = custom_legend_titles[[input$user_category]])
+  # 
+  #   # Make interactive plot
+  #   # Remove mode bar
+  #   my_plotly_plot <- ggplotly(my_plot) %>%
+  #     config(displayModeBar = FALSE)
+  # 
+  #   return(my_plotly_plot)
+  # })
 # # 
 # # }
 # 
@@ -71,7 +116,7 @@ my_server <- function(input, output) {
 #                                       cause_short == "A" | cause_short == "AC" ~ 'Accident',
 #                                       cause_short == "U" ~ 'Undetermined'))
 # 
-# 
+
 # 
 # race_bar_chart <- function() {all_deaths %>%
 #     ggplot(mapping = aes(x = race, fill = race)) +
@@ -91,13 +136,13 @@ my_server <- function(input, output) {
 # 
 # 
 # server <- function(input, output) 
-#   output$Plot <- renderPlotly ({race_bar_chart <- all_deaths %>%
-#     ggplot(mapping = aes(x = race, fill = race)) +
-#     labs(title = "Breakdown of Incarcerated Races", subtitle = "Who Have Died in Prison") +
-#     geom_bar() 
-#   Plot <- ggplotly(race_bar_chart) %>%
-#     config(displayModeBar = FALSE)
-#   return(Plot)})
+  # output$Plot <- renderPlotly ({race_bar_chart <- all_deaths %>%
+  #   ggplot(mapping = aes(x = race, fill = race)) +
+  #   labs(title = "Breakdown of Incarcerated Races", subtitle = "Who Have Died in Prison") +
+  #   geom_bar()
+  # Plot <- ggplotly(race_bar_chart) %>%
+  #   config(displayModeBar = FALSE)
+  # return(Plot)})
 # 
 # 
 # 
