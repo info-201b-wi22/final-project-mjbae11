30afed02ec012dc066b21acb1d6c2fcf011ca922
a-hsiao <ahsiao2@uw.edu>
2022-03-09 16:35:30 -0800

    added another widget and tried to add image and theme to intro

--- my_server.R
+++ my_server.R
@@ -3,7 +3,7 @@ library(plotly)
 
 # Data source: https://apreshill.github.io/data-vis-labs-2018/01-eda_hot_dogs.html
 
-hotdog_df <- read.csv("https://raw.githubusercontent.com/apreshill/data-vis-labs-2018/master/data/hot_dog_contest.csv")
+death_data <- read.csv("all_deaths.csv")
 
 custom_legend_titles <- reactiveValues("year" = "Year of Contest", "gender" = "Gender of Athlete", "name" = "Name of Athlete")
 
@@ -28,3 +28,42 @@ server <- function(input, output) {
   })
 
 }
+
+all_deaths <- all_deaths %>%
+  mutate(race_full = case_when(race == "B" ~ 'Black',
+                               race == "W" ~ 'white',
+                               race == "H" ~ 'Hispanic',
+                               race == "A" ~ 'Asian',
+                               race == "AI" ~ 'Native American/Indigenous',
+                               race == "O" ~ 'Other',
+                               race == "NA" ~ 'Not Stated'))
+
+
+
+race_bar_chart <- function() {all_deaths %>%
+    ggplot(mapping = aes(x = race, fill = race)) +
+    labs(title = "Breakdown of Incarcerated Races", subtitle = "Who Have Died in Prison") +
+    geom_bar() 
+  
+}
+
+Plot <- ggplotly(race_bar_chart)
+
+
+#Based on the printed bar chart, the majority of people who have died in prison 
+#are white. However, the population of white people in the US is and the incarcerated population is ___.
+#The population of 
+
+
+server <- function(input, output) 
+  output$Plot <- renderPlotly ({race_bar_chart <- all_deaths %>%
+    ggplot(mapping = aes(x = race, fill = race)) +
+    labs(title = "Breakdown of Incarcerated Races", subtitle = "Who Have Died in Prison") +
+    geom_bar() 
+  Plot <- ggplotly(race_bar_chart) %>%
+    config(displayModeBar = FALSE)
+  return(Plot)})
+
+
+
+
--- my_ui.R
+++ my_ui.R
@@ -4,9 +4,19 @@ library(plotly)
 intro_tab <- tabPanel(
   "Introduction",
   fluidPage(
-    p("This is a demo web app!")
-  )
-)
+    h1("Questions We Are Seeking To Answer", align = "center"), 
+    p("How many people who have died while being incarcerated who were not convicted?"), 
+    p("What correlations are there between cause of death and race, if any?"), 
+    p("What is the main cause of death?"), 
+    h1("Data We Are Using To Answer Our Questions"), 
+    p("We are using the dataset from Reuters Investigates. Reuters has filed over 1,500 public record requests to get data related to people dying while being incarcerated from 2008 to 2020. As Reuter notes, the U.S. Justice Department does not have this information publicly available, which necessitated Reuter finding the data on their own accord and having it available for public use"),
+    img(src='jail_cell.png', align = "center"),
+    theme = bs_theme(
+                  bootswatch = "darkly"),
+    h1("Issues That May Occur With The Data"),
+    p(""))
+    
+
 
 plot_sidebar <- sidebarPanel(
   selectInput(
@@ -27,32 +37,44 @@ plot_tab <- tabPanel(
   "Plot",
   sidebarLayout(
     plot_sidebar,
-    plot_main
-  )
-)
+    plot_main, sliderInput(inputId = "slider_year", label = "Years Selected", min = "2008", max = "2020")
+
+    
+  ))
+
 
 plot2_tab <- tabPanel(
   "Plot 2",
   sidebarLayout(
     plot_sidebar,
-    plot_main
+    plot_main,
+    (radioButtons(inputId = "unconvicted/convicted_button", label = "Conviction Status", choices = custody_status$C = "Convicted",
+                  custody_status$U = "Unconvicted"))
+    
   )
-)
+
 
 plot3_tab <- tabPanel(
   "Plot 3",
   sidebarLayout(
     plot_sidebar,
-    plot_main
-  )
-)
+    plot_main,
+    (checkboxGroupInput(inputId = "causes_of_death",
+                       label = "Cause of Death", choices = list(Medical = "Medical", Suicide = "Suicide", Drug_Abuse = "Drug Abuse", Homicide = "Homicide", Other = "Other", Accident = "Accident",
+                       Undetermined = "Undetermined"), selected, inline, width,
+                       choiceNames, choiceValues)
+  ))
+
+
+
+##^^might need to fix the code for the widget above later
 
 conclusion_tab <- tabPanel(
   "Conclusion",
   fluidPage(
-    p("This is the conclusion page.")
-  )
-)
+    h1("Conclusion"), p("INSERT TEXT DESCRIBING OUR FINDINGS AND WHAT WE CONCLUDE")
+  ))
+
 
 
 
